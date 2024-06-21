3a441f80f7e3638c324a4e9c73d680ae2fe6d4b7
Heshi Macharia <heshi.macharia@student.moringaschool.com>
2024-06-21 16:37:35 +0300

    anagrams

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, word_list):
+        matches = []
+        sorted_word = sorted(self.word)
+        
+        for word in word_list:
+            if word.lower() != self.word and sorted(word.lower()) == sorted_word:
+                matches.append(word)
+                
+        return matches
+    
+listen = Anagram("listen")
+listen.match(['enlists', 'google', 'inlets', 'banana'])
